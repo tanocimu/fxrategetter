97e894fdd9a3959cd57ef1762f30170ef4d90054
tanocimu <tanocimu@gmail.com>
2022-09-03 10:06:32 +0900

    initialized

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<head></head>
+
+<body>
+<?php
+//UPDATE exchange_rate SET rate = '140.210', updatetime = NOW() WHERE exchange_rate.id = 1;
+
+$link = mysqli_connect('localhost', 'id19476344_loser', ')l>?Gnj^Sv\lNh<6', 'id19476344_database');
+
+$echo_str = "";//レスポンス用の文字列格納用
+// 接続状況をチェックします
+if (mysqli_connect_errno()) {
+    die("cannot access db:" . mysqli_connect_error() . "\n");
+} else {
+    $echo_str = "DB OK.\n";
+}
+
+$value1 = '';
+$value1 = $_POST['value1'];
+$value1 = htmlspecialchars($value1);
+if(get_magic_quotes_gpc()) $value1 = stripslashes($value1);
+
+$echo_str = $echo_str . "$value1" . "\n";
+
+$value2 = '';
+$value2 = $_POST['value2'];
+$value2 = htmlspecialchars($value2);
+if(get_magic_quotes_gpc()) $value2 = stripslashes($value2);
+
+$echo_str = $echo_str ."$value2" . "\n";
+
+$query1 = 'UPDATE exchange_rate SET rate = "' .$value2. '", updatetime = NOW() WHERE exchange_rate.id = 1';
+if (mysqli_query($link, $query1)) {
+    $echo_str = $echo_str . "UPDATE success";
+}
+
+echo "$echo_str";
+
+mysqli_close($link);
+
+?>
+</body>
+
+</html>
